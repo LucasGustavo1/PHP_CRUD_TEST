411d96afe2fd7c6b68f22d182665b12b4c3db32c
Lucas Gustavo <lucas_gustavo2016@hotmail.com>
2023-06-25 17:46:59 -0300

    required

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -26,6 +26,15 @@
                     {{ session('erro') }}
                 </div>
             @endif
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
             <div class="card">
                 <div class="card-header">
                     <!-- Button trigger modal -->
